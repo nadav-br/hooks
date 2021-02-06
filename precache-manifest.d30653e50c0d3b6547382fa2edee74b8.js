d1acb51114abbce17ef18db4f42dea8c5d87d0d5
nadav-br <nadav.br@gmail.com>
2021-02-06 23:11:11 +0200

    Updates

---
+++ precache-manifest.d30653e50c0d3b6547382fa2edee74b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42b3bf2b738ff91d591c825e9f1522a",
+    "url": "/hooks/index.html"
+  },
+  {
+    "revision": "c853e8b5899c67f26fca",
+    "url": "/hooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7906c2515a93ce87963e",
+    "url": "/hooks/static/js/2.686174c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hooks/static/js/2.686174c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c853e8b5899c67f26fca",
+    "url": "/hooks/static/js/main.a95296e4.chunk.js"
+  },
+  {
+    "revision": "9db395dbfee17938b1c4",
+    "url": "/hooks/static/js/runtime-main.9683d882.js"
+  }
+]);
